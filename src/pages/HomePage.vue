3dc3a05a3fb5cb3287dd9ca0736033ef0621e08a
justin.dimichele <justindimichele@gmail.com>
2026-02-21 03:02:49 -0800

    Starting on basic skeleton

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>GameDiff</h1>
+  <p>
+    Has it been awhile since you've played {{ randomGameName }}? Want to catch up fast without
+    reading all the patch notes? We've got you covered.
+  </p>
+  <h4>We'll get started here, what's the game you want to play?</h4>
+  <input type="text" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      randomGameName: ['League of Legends', 'DotA2', 'Counter-Strike 2'],
+      gameName: '',
+    }
+  },
+  mounted() {
+    this.gameNameDisplay()
+  },
+  methods: {},
+}
+</script>
